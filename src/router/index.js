595177d87b9f3b4efd608587c5269e0342de176d
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-05-14 22:49:06 +0200

    Added router login and form vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Form from '@/components/form'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
@@ -10,6 +12,17 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/form',
+      name: 'Form',
+      component: Form
+    },
+    ,
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
     }
   ]
 })
