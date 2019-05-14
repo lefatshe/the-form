5e486e95ed317abf4f4c9fec0275debea310e675
Sechaba Mothobi <sechaba.mothobi@investec.co.za>
2019-05-14 23:49:51 +0200

    Added Materialse - formatted form vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Vuyo Funeral</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Menu 1</a></li>
+        <li><a href="badges.html">Menu 2</a></li>
+        <li><a href="collapsible.html">Menu 3</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      msg: 'Welcome to form'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -1,34 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="index container">
+    <!-- <h1>{{ msg }}</h1> -->
+    <div class="divider"></div>
+    <div class="section">
+      <div class="col s12">
+        <div class="row">
+          <div class="input-field col s6">
+            <input placeholder="Placeholder" id="first_name" type="text" class="validate">
+            <label for="first_name">First Name</label>
+          </div>
+          <div class="input-field col s6">
+            <input id="last_name" type="text" class="validate">
+            <label for="last_name">Last Name</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="section">
+      <h5>Section 2</h5>
+      <p>Add more</p>
+    </div>
+    <div class="divider"></div>
+    <div class="section">
+      <h5>Section 3</h5>
+      <p>Stuff</p>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Form',
-  data () {
-    return {
-      msg: 'Welcome to form'
+  export default {
+    name: 'Form',
+    data() {
+      return {
+        msg: 'Welcome to form'
+      }
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
